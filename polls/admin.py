07399e9893459d1d7c50c76b3781101061785f56
falcon92 <tktk.tktk.ixjs@gmail.com>
2020-08-03 13:33:14 +0900

    静的ファイルの追加と管理サイトのカスタマイズ

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 
-# Register your models here.
-admin.site.register(Question)
+# 管理サイトに表示
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+
+# 管理サイトでpub_dateの表示を先にする
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+
+# フィールドのタイトルを追加
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+
+# admin.site.register(Question, QuestionAdmin)
+
+
+# QuestionとChoiceを同時に登録
+class ChoiceInline(admin.TabularInline):
+# class ChoiceInline(admin.StackedInline):  # ページが長くなるので、Tabularに変更
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    # question_text, pub_dateなどを追加して表示
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    # 管理サイトの表示を修正
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = '直近に作成されたか？'
 
 
 class Choice(models.Model):
